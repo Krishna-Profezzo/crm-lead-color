4fcdc151f6fdb2a96691e5c72a419bb65d5a7441
MCHS24x7 <svk@SVKs-MacBook-Air.local>
2026-02-19 07:05:24 +0530

    fix: comment out app_include_js to resolve bench build error

--- lead_gender/hooks.py
+++ lead_gender/hooks.py
@@ -14,7 +14,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/lead_gender/css/lead_gender.css"
-app_include_js = "/assets/lead_gender/js/lead_gender.js"
+# app_include_js = "/assets/lead_gender/js/lead_gender.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/lead_gender/css/lead_gender.css"
